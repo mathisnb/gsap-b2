204e122712a1c55e9a8f228f88377449b609b499
Mathis Nicouleau <mathis.nb@orange.fr>
2025-01-17 13:31:07 +0100

    Adding GSAP

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//GSAP Modules
+gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin);
+
+//GSAP Demo animation
+gsap.to("#demo",{
+    rotation : 360,
+    duration : 3,
+})
