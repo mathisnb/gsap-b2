1f99edfe65ccf4c55281525946b07882be38f863
Mathis Nicouleau <mathis.nb@orange.fr>
2025-01-28 13:47:30 +0100

    2nd Section integration in html an css

--- js/main.js
+++ js/main.js
@@ -2,7 +2,21 @@
 gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin);
 
 //GSAP Demo animation
-gsap.to("#demo",{
-    rotation : 360,
+/*gsap.to("#introImageWrapper",{
+    motionPath : {
+        path : "M728.47,0S754.49,657.5,0,657.5",
+        autoRotate : false,
+    }
+    
+})*/
+
+/*ScrollTrigger : {
     duration : 3,
-})
+    trigger : '#section2',
+    start : 'top 60%',
+    end : 'top 20%',
+    markers : true,
+    id : '#introImageWrapper',
+    toggleActions : 'none reverse reset'
+    },*/
+
